187e9569eea27dd9205b53c6e8771fc822be79d4
Meng Li <meng.li@signavio.com>
2021-12-13 16:46:33 +0100

    configure a central location to manage dependencies

---
+++ app/src/test/java/TestA.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestA {
+	
+	
+	@Test
+	public void test1(){
+		System.out.println("Wow it worked");
+		Assertions.assertEquals(1,1);
+	}
+	
+}
---
+++ src/test/java/RandomTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RandomTest {
+	
+	
+	@Test
+	public void test1(){
+		System.out.println("Wow");
+		Assertions.assertEquals(1,1);
+	}
+	
+}
